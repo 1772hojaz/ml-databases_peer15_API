7f83472631246995fd989b447b8710c220eb26b9
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2025-03-13 03:08:05 +0200

    I, UPDATED #app/database/connection.py,#requirements.txt

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,15 +1,15 @@
-import pymysql
+import mysql.connector
+from mysql.connector import Error
 
 def get_db_connection():
     try:
-        connection = pymysql.connect(
+        connection = mysql.connector.connect(
             host="orgr9.h.filess.io",  # Database host
+            database="mlgroup_childrenof",  # Database name
             user="mlgroup_childrenof",  # Database username
             password="284b5b9f1bf250ee916853314f97240c8317249e",  # Database password
-            database="mlgroup_childrenof",  # Database name
-            port=3307,  # Database port
-            cursorclass=pymysql.cursors.DictCursor  # Use dictionary cursors
+            port=3307  # Database port
         )
         return connection
-    except pymysql.Error as e:
+    except Error as e:
         raise Exception(f"Database connection error: {str(e)}")
